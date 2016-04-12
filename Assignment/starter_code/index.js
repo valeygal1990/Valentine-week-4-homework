344202fef2fa0b7f068c3765d3f9967feb867658
valeygal1990 <valentinesakwa@Valentines-MacBook-Air.local>
2016-04-11 20:13:59 -0400

    second commit

--- Assignment/starter_code/index.js
+++ Assignment/starter_code/index.js
@@ -1,12 +1,35 @@
 $(document).ready(function())
 
-$('#show-this-on-click').hide();
-$('readmore').click(showReadMore);
+$('a.readless').hide();
+$('a.readmore').click(showReadMore);
 
 function showReadMore(){
-	event.preventDefault()
+	event.preventDefault();
 	$('#show-this-on-click').slideDown();
+	$('a.readmore').hide();
+	$('a.readless').show();
 }
+
+$('a.readless').click(hideShowThisOnClick)
+
+function hideShowThisOnClick(){
+	event.preventDefault();
+	$('#show-this-on-click').slideUp();
+	$('a.readless').hide();
+	$('a.readmore').show();
+}
+
+$('a.learnmore').click(learnMore);
+
+function learnMore(){
+	event.preventDefault();
+	$('#learnmoretext').slideDown();
+	$('a.learnmore').hide();
+}
+
+
+
+
 
 
 
